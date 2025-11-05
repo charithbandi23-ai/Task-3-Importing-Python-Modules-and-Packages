6a8ded86cb78c1dae1d98dad11ea9f1530b2b01c
charithbandi23-ai <charithbandi23@gmail.com>
2025-11-05 18:48:13 +0530

    push

---
+++ 3.1.py
@@ -0,0 +1,6 @@
+widgets = int(input("Enter the number of widgets: ")) 
+gizmos = int(input("Enter the number of gizmos: ")) 
+WIDGET_WEIGHT = 75 
+GIZMOS_WEIGHT = 112 
+total_weight = (widgets * WIDGET_WEIGHT) + (gizmos * GIZMOS_WEIGHT) 
+print("The total weight of all these widgets and gizmos is", total_weight, "grams.")
